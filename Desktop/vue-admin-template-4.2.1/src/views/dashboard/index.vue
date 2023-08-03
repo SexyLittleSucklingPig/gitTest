d229a1e6261304e0ffbe489e6518798cb31cfda0
SexyLittleSucklingPig <1946677404@qq.com>
2023-08-03 12:34:17 +0800

    '参考'

---
+++ Desktop/vue-admin-template-4.2.1/src/views/dashboard/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-text">name: {{ name }}</div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Dashboard',
+  computed: {
+    ...mapGetters([
+      'name'
+    ])
+  },
+  methods: {
+    filter (value) {
+      return value.replace(/[^0-9]/ig, '')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
